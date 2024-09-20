4f64bd7cbe6d61ea3bdc9c749c1e38b73965cf38
DarthZachTheWise <Zach.hamilton@kingsway.school.nz>
2024-09-20 14:42:56 +1200

    Add files via upload
    Created various variables.

---
+++ Variable.py
@@ -0,0 +1,32 @@
+# Showing how variables work
+# Atuhor: Zach Hamilton
+# Date: 20/09/24
+# vision 1
+
+# Differant types of numbers
+# Store a Number 
+
+num_1 = 1
+print(num_1)
+
+# Store a string 
+name = "Juergen"
+print(name)
+
+name = "aaskjdha "
+print(name)
+
+# Do calculations in a variable and store the results
+sum = 5 + 5
+print(sum)
+sum_string = "5 + 5 = 10"
+print(sum_string)
+
+# Adding strings together is called concatination
+concatination =  name + sum_string
+print(concatination)
+
+
+
+square_num = 2^2
+print(square_num)
