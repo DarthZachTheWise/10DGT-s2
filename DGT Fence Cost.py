22603ec6fcda3094c3a476ed07823dec223434f1
Zach Hamilton <Zach.hamilton@kingsway.school.nz>
2024-11-06 20:16:17 +1300

    DGT Fence Cost

--- DGT Fence Cost.py
+++ DGT Fence Cost.py
@@ -19,7 +19,7 @@ def vaild_number(question):
             print(value_error)
     return response
 
-def vaild_gate(question):
+def vaild_int(question):
     error_zero_negitive = "Your responce is an invalid number."
     value_error = "Your responce is an invalid number."
     while True:
@@ -38,11 +38,11 @@ keep_going = ""                                                                 
                                                                                                     
 while keep_going == "":                                                                             # Starts the while loop. This is the main section of the code. 
 
-    fence_length = vaild_number("\nWhat is the length of the fence in meters?\n")                     # Asks the user for the dimention and cost of the fence per meter.
+    fence_length = vaild_number("\n\nWhat is the length of the fence in meters?\n")                     # Asks the user for the dimention and cost of the fence per meter.
     fence_width = vaild_number("\nWhat is the width of the fence in meters?\n")                       
     fence_cost = vaild_number("\nWhat is the cost of the fence per meter?\n$")
     
-    gate_number = vaild_gate("\nHow many gates does the fence have?\n")                               # Asks the user how many gates their fence has. If there are zero fences the code will skip the rest of the gate question. 
+    gate_number = vaild_int("\nHow many gates does the fence have?\n")                               # Asks the user how many gates their fence has. If there are zero fences the code will skip the rest of the gate question. 
     
     if  gate_number > 0:                                                                            # If the number of gates that are in bigger than zero than it will go through this if statement. 
         gate_cost = vaild_number("\nHow much does your gate cost?\n$")                                # Asks the user for the cost and length of the fence. 
@@ -61,12 +61,14 @@ while keep_going == "":                                                         
             
 
         else:   
-            print (f"The total cost of the fence will be ${fence_total_cost_gate} including the cost of the gates.")         # Tells the user the total cost of the total. 
-            keep_going = input("What you like to go again? If so than press <enter> otheriwse click any other key to stop.") # Asks the user if they would like to loop. 
+            print (f"\nThe total cost of the fence will be ${fence_total_cost_gate} including the cost of the gates.")         # Tells the user the total cost of the total. 
+            keep_going = input("\nWhat you like to go again? If so than press <enter> otheriwse click any other key to stop.") # Asks the user if they would like to loop. 
+            print("___________________________________________________________________________________________")
     
     else:                                                                                                                    # This is the code that doesn't give the user questions about the gates. 
         fence_perimeter_nogate = (fence_length * 2) + (fence_width * 2)                                                      # Finds the perimeter of the fence.
         fence_total_cost_nogate = (fence_perimeter_nogate * fence_cost)                                                      # Finds the cost of the fence not including the gates. 
 
         print(f"The total cost of your fence is ${fence_total_cost_nogate}.")                                                # Tells the user the total fence cost. 
-        keep_going = input("What you like to go again? If so than press <enter> otheriwse click any other key to stop. ")    # Asks the user if they want the code go again. 
+        keep_going = input("What you like to go again? If so than press <enter> otheriwse click any other key to stop.")    # Asks the user if they want the code go again. 
+        print("___________________________________________________________________________________________")
