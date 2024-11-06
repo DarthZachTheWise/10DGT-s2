d1d22449891eb416880826765315870b88b62f0e
Zach Hamilton <Zach.hamilton@kingsway.school.nz>
2024-11-06 19:35:26 +1300

    Area calculator advanced

--- Area calculator advanced.py
+++ Area calculator advanced.py
@@ -26,68 +26,56 @@ def vaild_number(question):
 
 
 
-print("This is a calculater that find the area and somtimes the perimeter of the shapes")
+print("\nThis is a calculater that find the area and somtimes the perimeter of the shapes")
 keep_going = ""                     # "keep_going is the variable that if you changed  will exit the program the while loop."
 
 while keep_going == "":             # This is the main while loop.
                 
-    selection = vaild_number("Select your shape\nPress 1 for rectangle\nPress 2 for triangle\nPress 3 for circle\n")    # # These print statments tell the user what key to press for each shape. The variable controls which shape the calculater calculates.
+    selection = vaild_number("\nSelect your shape\n\nPress 1 for rectangle\nPress 2 for triangle\nPress 3 for circle\n")    # # These print statments tell the user what key to press for each shape. The variable controls which shape the calculater calculates.
 
     if selection == 1:
-        print("You selected rectangle.")                                    # Lets the user know what shape they chose.
+        print("\nYou selected rectangle.")                                    # Lets the user know what shape they chose.
 
-        length = vaild_number("What is the length of your rectangle?\n")        # Asks the user to give length of the rectangle. 
-        print(f"The length is {length} units.")                             # Give infomation back to the user. 
+        length = vaild_number("\nWhat is the length of your rectangle?\n")        # Asks the user to give length of the rectangle. 
+        print(f"\nThe length is {length} units.")                             # Give infomation back to the user. 
 
-        height = vaild_number("What is the width of your rectangle?\n")         # Asks the user to give width of the rectangle.
-        print(f"The height is {height} units.")                             # Give infomation back to the user. 
+        height = vaild_number("\nWhat is the width of your rectangle?\n")         # Asks the user to give width of the rectangle.
+        print(f"\nThe height is {height} units.")                             # Give infomation back to the user. 
 
         rect_perimeter = (length * 2) + (height * 2)                        # Calculates the perimeter of the rectangle.
         area =  length * height                                             # Calculates the area of the rectangle.
 
-        print (f"Perimeter = {rect_perimeter} units.\n")                      # Tell the user the perimeter of the rectangle.
-        print (f"Area = {area} units squared.")                             # Tell the user the area of the rectangle.
+        print (f"\nPerimeter = {rect_perimeter} units.")                      # Tell the user the perimeter of the rectangle.
+        print (f"\nArea = {area} units squared.")                             # Tell the user the area of the rectangle.
        
-        keep_going = input("Press enter to calculate another area. Else type any other key to stop.")       # Lets the user break from the loop.
+        keep_going = input("\nPress enter to calculate another area. Else type any other key to stop.")       # Lets the user break from the loop.
 
     elif selection == 2:                                                                                    
-        print ("You selected triangle.")                                                                    # Lets the user know what shape they chose.
+        print ("\nYou selected triangle.")                                                                    # Lets the user know what shape they chose.
         
-        tri_base_length = vaild_number("What is the length of the base of your triangle?\n")                  # Asks the user for the length of the base of the triangle.
-        print (f"The height is {tri_base_length} units")                                                   # Give infomation back to the user.
+        tri_base_length = vaild_number("\nWhat is the length of the base of your triangle?\n")                  # Asks the user for the length of the base of the triangle.
+        print (f"\nThe height is {tri_base_length} units")                                                   # Give infomation back to the user.
 
-        Tri_heigth = vaild_number("What is the height of your tirangle?\n")                                   # Asks the user for the height of the triangle.
-        print(f"The height is {Tri_heigth} units.\n")                                                         # Give infomation back to the user.
+        Tri_heigth = vaild_number("\nWhat is the height of your tirangle?\n")                                   # Asks the user for the height of the triangle.
+        print(f"\nThe height is {Tri_heigth} units.")                                                         # Give infomation back to the user.
 
         Tri_area = (Tri_heigth * tri_base_length)/2                                                         # Calculates the area of the triangle
 
-        print(f"Area = {Tri_area} units squared.")                                                          # Tell the user the area of the triangle
-        keep_going = input("Press enter to calculate another area. Else type any other key to stop.")       # Lets the user break from the loop.
+        print(f"\nArea = {Tri_area} units squared.")                                                          # Tell the user the area of the triangle
+        keep_going = input("\nPress enter to calculate another area. Else type any other key to stop.")       # Lets the user break from the loop.
 
-    elif selection == 3:                                                                                    # This elif does bacily the same as the last too just for the circle. 
-        cir_radius  = vaild_number("What is the radius of the circle?\n")
-        print(f"The radius of your circle is {cir_radius} units.\n")                           
+    elif selection == 3:   
+        print("\nYou selected circle.")                                                                                 # This elif does bacily the same as the last too just for the circle. 
+        cir_radius  = vaild_number("\nWhat is the radius of the circle?\n")
+        print(f"\nThe radius of your circle is {cir_radius} units.\n")                           
         
         cir_area = (cir_radius* PI)**2                                                                      
         cir_perimeter = 2 * PI * cir_radius
 
-        print(f"Area = {cir_area} units squared.")                                       
-        print(f"Circumference = {cir_perimeter} units.")  
+        print(f"\nArea = {cir_area} units squared.")                                       
+        print(f"\nCircumference = {cir_perimeter} units.")  
                                        
-        keep_going = input("Press enter to calculate another area. Else type any other key to stop.")       
+        keep_going = input("\nPress enter to calculate another area. Else type any other key to stop.")       
 
     else:                                                                                                   
-        print("Your responce is an invalid input. Try again.")                                              # If the user gets to the else something went wrong and it allow them to try again. 
-
-    
-
-
-
-
-
-
-
-    
-    
-
-
+        print("\nYour responce is an invalid input. Try again.")                                              # If the user gets to the else something went wrong and it allow them to try again. 
