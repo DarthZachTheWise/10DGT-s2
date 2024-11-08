291493065e9ef867c442d5edd837c0bbd6e63f74
Zach Hamilton <Zach.hamilton@kingsway.school.nz>
2024-11-08 15:08:13 +1300

    Area calculator advanced

--- Area calculator advanced.py
+++ Area calculator advanced.py
@@ -10,8 +10,8 @@
 # This checks whether the numbe that the user inputs is a number that the code can use.
 PI = 3.1415926533
 def vaild_number(question):
-    error_zero_negitive = "Your responce is an invalid number. Try again"
-    value_error = "Your responce is an invalid number."
+    error_zero_negitive = "\nYour responce is an invalid number. Try again.\n"
+    value_error = "\nYour responce is an invalid number.\n"
     while True:
         try:
             response = float(input(question))
@@ -34,7 +34,7 @@ while keep_going == "":             # This is the main while loop.
     selection = vaild_number("\nSelect your shape\n\nPress 1 for rectangle\nPress 2 for triangle\nPress 3 for circle\n")    # These print statments tell the user what key to press for each shape. The variable controls which shape the calculater calculates.
 
     if selection == 1:
-        print("\nYou selected rectangle.")                                    # Lets the user know what shape they chose.
+        print("\nYou selected rectangle.\n")                                    # Lets the user know what shape they chose.
 
         length = vaild_number("\nWhat is the length of your rectangle?\n")    # Asks the user to give length of the rectangle. 
          
@@ -46,10 +46,10 @@ while keep_going == "":             # This is the main while loop.
         print (f"\nPerimeter = {rect_perimeter} units.")                      # Tell the user the perimeter of the rectangle.
         print (f"\nArea = {area} units squared.")                             # Tell the user the area of the rectangle.
        
-        keep_going = input("\nPress enter to calculate another area. Else type any other key to stop.")       # Lets the user break from the loop.
+        keep_going = input("\n\nPress enter to calculate another area. Else type any other key to stop.")       # Lets the user break from the loop.
 
     elif selection == 2:                                                                                    
-        print ("\nYou selected triangle.")                                                                    # Lets the user know what shape they chose.
+        print ("\nYou selected triangle.\n")                                                                    # Lets the user know what shape they chose.
         
         tri_base_length = vaild_number("\nWhat is the length of the base of your triangle?\n")                # Asks the user for the length of the base of the triangle.
        
@@ -58,10 +58,10 @@ while keep_going == "":             # This is the main while loop.
         Tri_area = (Tri_heigth * tri_base_length)/2                                                           # Calculates the area of the triangle
 
         print(f"\nArea = {Tri_area} units squared.")                                                          # Tell the user the area of the triangle
-        keep_going = input("\nPress enter to calculate another area. Else type any other key to stop.")       # Lets the user break from the loop.
+        keep_going = input("\n\nPress enter to calculate another area. Else type any other key to stop.")       # Lets the user break from the loop.
 
     elif selection == 3:   
-        print("\nYou selected circle.")                                                                       # This elif does bacily the same as the last two just for the circle. 
+        print("\nYou selected circle.\n")                                                                       # This elif does bacily the same as the last two just for the circle. 
         cir_radius  = vaild_number("\nWhat is the radius of the circle?\n")
                                    
         
@@ -71,7 +71,7 @@ while keep_going == "":             # This is the main while loop.
         print(f"\nArea = {cir_area} units squared.")                                       
         print(f"\nCircumference = {cir_perimeter} units.")  
                                        
-        keep_going = input("\nPress enter to calculate another area. Else type any other key to stop.")       
+        keep_going = input("\n\nPress enter to calculate another area. Else type any other key to stop.")       
 
     else:                                                                                                   
         print("\nYour responce is an invalid input. Try again.")                                              # If the user gets to the else something went wrong and it allow them to try again. 
